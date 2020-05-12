aa32f44f59623f8624cd4f13f9cc45a9dddf200b
Agushariadi28 <64575762+Agushariadi28@users.noreply.github.com>
2020-05-12 21:26:47 +0800

    Create jobsheet 2 C++.cpp
    Buatlah program mengisi dan menampilkan elemen array denan index array sesuai dengan NIM terakhir setiap Mahasiswa

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main (){
+int x[10];
+
+cout <<"masukan nilai 1: "; 
+cin>>x[0];;
+cout <<"masukan nilai 2: "; 
+cin>>x[1];
+cout <<"masukan nilai 3: "; 
+cin>>x[2];
+cout <<"masukan nilai 4: "; 
+cin>>x[3];
+cout <<"masukan nilai 5: "; 
+cin>>x[4];
+cout <<"masukan nilai 6: "; 
+cin>>x[5];
+cout <<"masukan nilai 7: "; 
+cin>>x[6];
+cout <<"masukan nilai 8: "; 
+cin>>x[7];
+cout <<"masukan nilai 9: "; 
+cin>>x[8];
+cout <<"masukan nilai 10: "; 
+cin>>x[9];
+cout<<endl;
+cout<<"nilai ke-1="<<x[0]<<endl;
+cout<<"nilai ke-2="<<x[1]<<endl;
+cout<<"nilai ke-3="<<x[3]<<endl;
+cout<<"nilai ke-4="<<x[4]<<endl;
+cout<<"nilai ke-5="<<x[5]<<endl;
+cout<<"nilai ke-6="<<x[6]<<endl;
+cout<<"nilai ke-7="<<x[7]<<endl;
+cout<<"nilai ke-8="<<x[7]<<endl;
+cout<<"nilai ke-9="<<x[8]<<endl;
+cout<<"nilai ke-10="<<x[9]<<endl;
+system ("pause");	
+
+}
