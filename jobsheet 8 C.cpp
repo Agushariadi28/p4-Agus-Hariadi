63b9c0f051c8255702c9799f1c2196f623084714
Agushariadi28 <64575762+Agushariadi28@users.noreply.github.com>
2020-05-16 13:25:02 +0800

    Create jobsheet 8 C.cpp
    Membuat program mengisi nilai array dan menampilkanya dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap Mahasiswa

---
+++ jobsheet 8 C.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+int x[10],i;
+printf("masukan 2 angka nim terakhir anda\n\n");
+for(i=0;i<=4;i++)
+{
+printf("Masukkan nilai %i : ",i+1);
+scanf("%i",&x[i]);
+}
+printf("\n");
+for(i=0;i<=4;i++)
+{
+printf("nim ke-%i= %i\n",i+1,x[i]);
+}
+printf("\n");
+return 0;
+}
+
